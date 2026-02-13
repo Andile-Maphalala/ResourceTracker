4ec4bc7f053424b8159b729f3958b03edfd60f2d
Andile-Maphalala <andile.maphalala.am@gmail.com>
2026-02-13 20:36:48 +0200

    Added buildplan controller endpoints

---
+++ ResourceTracker.Api/Controllers/BuildPlanController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ResourceTracker.Application.Features.Commands.BuildPlanCommands.CreateBuildPlan;
+using ResourceTracker.Application.Features.Commands.BuildPlanCommands.DeleteBuildPlan;
+using ResourceTracker.Application.Features.Commands.BuildPlanCommands.UpdateBuildPlan;
+using ResourceTracker.Application.Features.Queries.BuildPlanQueries.GetBuildPlan;
+using ResourceTracker.Application.Features.Queries.BuildPlanQueries.SearchBuildPlans;
+
+namespace ResourceTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BuildPlanController(ISender sender) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CreateBuildPlan([FromBody] CreateBuildPlanCommand request)
+        {
+            var response = await sender.Send(request);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateBuildPlan([FromBody] UpdateBuildPlanCommand request)
+        {
+            await sender.Send(request);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteBuildPlan([FromBody] DeleteBuildPlanCommand request)
+        {
+            await sender.Send(request);
+            return NoContent();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBuildPlan([FromQuery] GetBuildPlanQuery request)
+        {
+            var response = await sender.Send(request);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SearchBuildPlan([FromQuery] SearchBuildPlansQuery request)
+        {
+            var response = await sender.Send(request);
+            return Ok(response);
+        }
+    }
+}
