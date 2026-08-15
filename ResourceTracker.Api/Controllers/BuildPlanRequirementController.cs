032f025e9884521275d351e5e9a9e58ac6c8ae51
Andile-Maphalala <andile.maphalala.am@gmail.com>
2026-08-15 18:33:47 +0200

    Added buildplanrequermentc controller

---
+++ ResourceTracker.Api/Controllers/BuildPlanRequirementController.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ResourceTracker.Application.Features.Queries.BuildPlanRequirementQueries.GetBuildPlanRequirement;
+
+namespace ResourceTracker.Api.Controllers
+{
+
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BuildPlanRequirementController(ISender sender) : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(GetBuildPlanRequirementsResponse), StatusCodes.Status200OK)]
+        public async Task<ActionResult<GetBuildPlanRequirementsResponse>> GetBuildPlanRequirement(int buildPlanId, bool includeFacilityRequirements, bool IncludeInventory, CancellationToken cancellationToken)
+        {
+            GetBuildPlanRequirementQuery request = new GetBuildPlanRequirementQuery
+            {
+                BuildPlanId = buildPlanId,
+                IncludeFacilityRequirements = includeFacilityRequirements,
+                IncludeInventory = IncludeInventory
+            };
+            var response = await sender.Send(request, cancellationToken);
+            return Ok(response);
+        }
+    }
+}
